d129ff48d040f8bbe4ba7a310fbabc1677abf8e6
Process64 <46932372+Process64@users.noreply.github.com>
2020-01-26 23:57:20 +0300

    Update Euler.java

--- Euler.java
+++ Euler.java
@@ -1,4 +1,4 @@
-public class Multiples {
+ public class Multiples {
 
     public static int multiplesSum(int n1, int n2, int limit) {
         int sum = 0;
@@ -111,3 +111,177 @@ public class FibonacciTest {
 
 }
 //3////////////////////////////////////////////////////////////////////
+public class Prime {
+    public static void main(String[] args) {
+        double out = 0;
+        double m = 600851475143d;
+        for (double n = 3; n < m; n += 2) {
+            while (m % n == 0) {
+                out = n;
+                m = m / n;
+            }
+        }
+        System.out.println("" + ((m == 1)?out:m));
+    }
+}
+//3////////////////////////////////////////////////////////////////////
+package findlaragestprimefactor;
+
+public class FindLaragestPrimeFactor{
+
+    boolean isPrime(long number) {
+        for (long divider = 2; divider <= number / 2; divider++) {
+            if (number % divider == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    void calculateLargestPrimeFactor() {
+        long largestPrimeFactor = 0;
+        long x = 600851475143L;
+        for(long factor = 3 ; factor <= x/2 ; factor = factor + 2){
+            if(x%factor==0 & factor>largestPrimeFactor & isPrime(factor)){
+                largestPrimeFactor = factor;
+            }
+        }
+        System.out.println(largestPrimeFactor);
+    }
+  public static void main(String[] args) {
+
+        MyProject m = new MyProject();
+        m.calculateLargestPrimeFactor();
+    }
+}
+//3////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+
+   class Primefactor
+            {
+                   public static void main(String args[])
+                       {
+                Scanner get=new Scanner(System.in);
+                System.out.println("Enter a number");
+                long number=get.nextLong();
+                int count=0;
+                long input=number;
+                      for(long i=number;i>=1;number--)
+                          {
+             for(long j=number;j>=1;j--)
+              {
+                if(i%j==0)
+                  {
+                     count++;
+                  }
+               if(count==2)
+                 {
+                    if(input%j==0)
+                       {
+                          System.out.println(j);
+                        }
+                  }
+              }
+           }
+     }
+   }
+//4////////////////////////////////////////////////////////////////////
+/**
+ * A simple implementation to solve Euler Problem #4 - Largest Palindrome Product.
+ */
+public class EulerProblemSolution {
+    public static void main(String[] args) {
+        int maxNumber = 999;
+        long maxAnswer = 0;
+        for(int i = maxNumber; i >=0; i--) {
+            for(int j = maxNumber; j >= 0; j--) {
+                long possibleAnswer = i * j;
+                if(possibleAnswer == reverse(possibleAnswer) && possibleAnswer > maxAnswer) {
+ maxAnswer =  possibleAnswer;
+                }
+            }
+        }
+        System.out.println(maxAnswer);
+    }
+    public static long reverse(long n)  {
+        long reverse =  0;
+        while ( n !=  0)  {
+            long lastDigit =  n %  10 ;
+ reverse =  reverse *  10  +  lastDigit;
+ n / = 10;
+        }
+        return reverse;
+    }
+}
+//4////////////////////////////////////////////////////////////////////
+public class Palindrome {
+public static void main (String [] args) {
+
+   int largestnum = 0;
+   Palindrome pp = new Palindrome();
+
+   for (int i = 100; i <= 999; i++) {
+       for (int k = 100; k <= 999; k++) {
+            if (pp.checkPal(i * k) == true) {
+                largestnum = i * k;
+            }
+        }
+    }
+
+   System.out.println(largestnum);
+}
+
+
+public boolean checkPal(int y) {
+    String snum = Integer.toString(y);
+    int snum_length = snum.length() - 1;
+    String palindrome = new String("");
+
+    for (int i = snum_length; i >= 0; i--) {
+        palindrome = palindrome.concat(Character.toString(snum.charAt(i)));
+    }
+
+    if (snum.equals(palindrome)) {
+        return true;
+    }
+    else {
+        return false;
+        }
+    }
+}
+//4////////////////////////////////////////////////////////////////////
+public static boolean isPalindrome(int number){
+    //checks to see if a number is a palendrome. (can't start with 0)
+    String numstring = Integer.toString(number);
+    char[] strarr = new char[String.valueOf(number).length()+1];
+    int x = 0;
+    int y = String.valueOf(number).length()-1;
+    for (int i=0;i<y+1;i++){
+        strarr[i] = numstring.charAt(i);
+    }
+    for (int a=0;a<String.valueOf(number).length()-1;a++){
+        if (strarr[x]!=strarr[y]){
+            return(false);
+        }
+        x++;
+        y--;
+    }
+    return(true);
+}
+public static int Problem4(){
+    int pp = 0;
+    int a = 0;
+    for (int i=100;i<1000;i++){
+        for (int j=100;j<1000;j++){
+            pp = i*j;
+            if (isPalindrome(pp)){
+                if (pp>a){
+                    a = pp;
+                }
+            }
+        }
+    }
+    return(a);
+}
+//4////////////////////////////////////////////////////////////////////
