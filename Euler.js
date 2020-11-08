38534f8e512a6285879f565b3678193c1096f029
Pasha <46932372+Process64@users.noreply.github.com>
2020-11-08 15:40:57 +0300

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -1,17 +1,6 @@
 //1//////////////////////////////////////////////////////////////////////////
-var n = 1000;
-
-var multiplesOfThree        = Math.floor((n - 1) /  3);
-var multiplesOfFive         = Math.floor((n - 1) /  5);
-var multiplesOfThreeAndFive = Math.floor((n - 1) / 15);
-
-var sum =  3 * multiplesOfThree        * (multiplesOfThree        + 1) / 2
-        +  5 * multiplesOfFive         * (multiplesOfFive         + 1) / 2
-        - 15 * multiplesOfThreeAndFive * (multiplesOfThreeAndFive + 1) / 2;
-console.log(sum);
-//1////////////////////////////////////////////////////////////////////////////
-var num = 1000
-var numbers = []
+let num = 1000
+let numbers = []
 
 for(var i = 0;  i < num; i++){
   if( i % 3 === 0|| i % 5 === 0){
@@ -19,32 +8,15 @@ for(var i = 0;  i < num; i++){
   }
 };
 
-var sum = numbers.reduce((a, b) => a + b, 0);
+let sum = numbers.reduce((a, b) => a + b, 0);
 console.log(sum);
-//2////////////////////////////////////////////////////////////////////////////
-function f() {
-  var fib = [];
-  fib.push(1,1);
-  var i = fib.length;
-  var total = 0;
-  while(fib[i-1] < 4000000) {
-    var x = fib[i-2] + fib [i-1];
-    fib.push(x);
-    i = fib.length;
-    if(x % 2 == 0) {
-      total += x;
-    }
-  } return total;
-}
-
-console.log(f()); //4613732
 //2///////////////////////////////////////////////////////////////////////////
 function f() {
-    var f1 = 1;
-    var f2 = 1;
-    var total = 0;
+    let f1 = 1;
+    let f2 = 1;
+    let total = 0;
     while (f2 < 4000000) {
-        var t = f1 + f2;
+        let t = f1 + f2;
         if (t % 2 == 0)
             total += t;
         f1 = f2;
@@ -54,3 +26,16 @@ function f() {
 }
 console.log(f()); //4613732
 //3/////////////////////////////////////////////////////////////////////////
+const originalTarget = target = 600851475143;
+let i = 2;
+while(i < target) {
+	while(target % i === 0) {
+		(function(newtarget) {
+			console.log(target  + i + newtarget);
+			target = newtarget;
+		})(target / i)
+	}
+	i++;
+}
+console.log(target + originalTarget); 
+//4//////////////////////////////////////////////////////////////////////////
