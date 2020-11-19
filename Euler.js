c80eae8504c6dccc3cbdd15151e05f87815ebb9a
Pasha <46932372+Process64@users.noreply.github.com>
2020-11-19 19:38:26 +0300

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -290,3 +290,32 @@ function problem11() {
 
 console.log(problem11());
 //12///////////////////////////////////////////////////////////////////
+var x = 0, y = 1;
+
+while (factors(x).length <= 500) {
+	x += y;
+  	y++;
+}
+
+console.log(x);
+
+function factors(n) {
+  let arr = [], i = 1, max = n;
+
+  while (i < max) {
+    if (n % i === 0) {
+      arr.push(i);
+
+      let k = n / i; 
+      if (i !== k) {
+        arr.push(k);
+      }
+      max = k;
+    }
+
+    i++;
+  }  
+
+  return arr.sort((a, b) => a - b);
+}
+//13///////////////////////////////////////////////////////
