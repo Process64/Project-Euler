7bc052e2687004fe25d9fbe805ec263eb333e5c9
Pasha <46932372+Process64@users.noreply.github.com>
2020-12-08 22:08:50 +0300

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -473,3 +473,32 @@ for (var i = 1, c = 1; i <= n; i++)
 }
 console.log(solution(20));
 //16///////////////////////////////////////////////////
+let number = [1],
+    sum = 0;
+
+for(let i = 0; i < 1000; i++)
+{
+    let overflow = 0,
+        count = number.length + 1
+    for(let j = 0; j < count; j++)
+    {
+        let digit = number[j] || 0;
+        digit = 2 * digit + overflow;
+        if(digit > 9)
+        {
+            digit -= 10;
+            overflow = 1;
+        }
+        else
+        {
+            overflow = 0;
+        }
+	number[j] = digit;
+    }
+}
+for(let i = 0; i < 1000; i++)
+{
+    sum += number[i];
+}
+console.log(sum);
+//17///////////////////////////////////////////////////
